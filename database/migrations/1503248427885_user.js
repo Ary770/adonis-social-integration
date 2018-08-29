b1bc80cb0eee1026bfe06b07545a46769423c32a
github username <github email address>
2018-08-29 14:23:11 -0400

    deleted unused files, edited Users table and migrated

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -6,9 +6,13 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+      table.string('name').nullable()
+      table.string('avatar').nullable()
+      table.string('username', 80).nullable()
+      table.string('email', 254).nullable()
+      table.string('provider_id').nullable()
+      table.string('provider').nullable()
+      table.string('password', 60).nullable()
       table.timestamps()
     })
   }
